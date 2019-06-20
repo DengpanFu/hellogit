7252e41c9335225459214c77bc5ea61a8ab5e25a
Dengpan Fu (FA Talent) <v-defu@microsoft.com>
2019-06-20 15:50:22 +0800

    test

--- temp.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env python
-# -*- coding: utf-8 -*-
-# @Date    : 2018-09-15 23:17:08
-# @Author  : Dengpan Fu (v-defu@microsoft.com)
-# @Link    : None
-# @Version : 0.0
-
-import os, sys
-import numpy as np
